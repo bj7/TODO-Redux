7cbef4bfe6550edddd04d018b003f458451550e2
Josh Bernitt <jjbernitt@gmail.com>
2017-12-23 12:48:22 -0500

    Vue cli
    Quick start vue

---
+++ todo-redux/src/components/Todo.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TODO',
+  data () {
+    return {
+      msg: 'TODO App'
+    }
+  }
+}
+</script>
